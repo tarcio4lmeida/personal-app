8536a267a5bd8641945461e8afdf284e7fc8d95e
tarcio almeida <tarcioalmeidalima@gmail.com>
2026-01-01 18:40:08 -0300

    Setup project

---
+++ backend/src/test/java/com/example/personal_api/PersonalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
